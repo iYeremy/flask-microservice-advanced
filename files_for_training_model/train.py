4b9039d244adf6945e04e830e4f469baa2056362
Yeremy <yeremyvegap@gmail.com>
2025-11-29 13:59:53 -0500

    refactor: adaptar train.py al nuevo dominio (de salud a plantas)

--- files_for_training_model/train.py
+++ files_for_training_model/train.py
@@ -1,38 +1,32 @@
 # https://youtu.be/bluclMxiUkA
 """
-La regresión lineal múltiple utiliza varias variables explicativas para predecir el resultado de una variable respuesta.
-Existen muchas variables y la regresión lineal múltiple está diseñada para crear un modelo
-basado en todas estas variables.
+Prototipo de entrenamiento para el simulador de cultivo urbano.
+Usamos un conjunto de datos sintético que representa parcelas de prueba donde se ajustan
+las horas de luz solar recibida, el nivel de riego/aspersión y el puntaje de crecimiento
+obtenido en cada cultivo.
 
-#Enlace del conjunto de datos:
-https://cdn.scribbr.com/wp-content/uploads//2020/02/heart.data_.zip?_ga=2.217642335.893016210.1598387608-409916526.1598387608
-
-#Enfermedad cardíaca
-El efecto que las variables independientes biking y smoking
-tienen sobre la variable dependiente heart disease
-
-el porcentaje de personas que van al trabajo en bicicleta cada día, el porcentaje de personas que fuman,
-y el porcentaje de personas con enfermedad cardíaca en una muestra imaginaria de 500 ciudades.
-
-
+La regresión lineal múltiple nos permite estimar el crecimiento esperado a partir de ambos
+parámetros de control y funcionará como base hasta contar con mediciones reales.
 """
 
+from pathlib import Path
 import pandas as pd
 import seaborn as sns
 import numpy as np
 
-df = pd.read_csv('heart_data.csv')
+BASE_DIR = Path(__file__).parent
+df = pd.read_csv(BASE_DIR / 'growth_data.csv')
 print(df.head())
 
-df = df.drop("Unnamed: 0", axis=1)
-#Algunas gráficas en Seaborn para entender los datos
+df = df.drop("plot_id", axis=1)
+#Algunas gráficas en Seaborn para entender el comportamiento del crecimiento
 
-sns.lmplot(x='biking', y='heart.disease', data=df)  
-sns.lmplot(x='smoking', y='heart.disease', data=df)  
+sns.lmplot(x='sunlight_hours', y='growth_score', data=df)  
+sns.lmplot(x='watering_level', y='growth_score', data=df)  
 
 
-x_df = df.drop('heart.disease', axis=1)
-y_df = df['heart.disease']
+x_df = df.drop('growth_score', axis=1)
+y_df = df['growth_score']
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(x_df, y_df, test_size=0.3, random_state=42)
@@ -43,7 +37,7 @@ from sklearn import linear_model
 model = linear_model.LinearRegression()
 
 #Ahora llamamos al método fit para entrenar el modelo usando las variables independientes.
-#Y el valor que se debe predecir (Images_Analyzed)
+#Y el valor que se debe predecir (puntaje de crecimiento estimado)
 
 model.fit(X_train, y_train) #Variables independientes, variable dependiente a predecir
 print(model.score(X_train, y_train))  #Imprime el valor R^2, una medida de qué tan bien
@@ -61,9 +55,9 @@ print(model.predict([[20.1, 56.3]]))
 
 
 #El modelo está listo. Veamos los coeficientes, almacenados como reg.coef_.
-#Estos son a, b y c de nuestra ecuación. 
+#Representan la influencia de cada control sobre el crecimiento estimado.
 #La intersección se almacena como reg.intercept_
 #print(model.coef_, model.intercept_)
 
-#Todo listo para predecir el número de imágenes que alguien analizaría en un momento dado
-#print(model.predict([[13, 2, 23]]))
+#Ejemplo rápido para pronosticar el crecimiento de una parcela hipotética
+#print(model.predict([[13, 2]]))
